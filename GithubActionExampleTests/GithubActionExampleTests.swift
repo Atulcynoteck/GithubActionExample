220fe027dcc40c5d458aef10f27a11d95a31bb60
Atulcynoteck <atul.tiwari@cynoteck.com>
2025-06-25 15:36:19 +0530

    Initial commit

---
+++ GithubActionExampleTests/GithubActionExampleTests.swift
@@ -0,0 +1,17 @@
+//
+//  GithubActionExampleTests.swift
+//  GithubActionExampleTests
+//
+//  Created by user on 25/06/25.
+//
+
+import Testing
+@testable import GithubActionExample
+
+struct GithubActionExampleTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
